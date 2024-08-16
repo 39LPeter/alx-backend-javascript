ddf2c5c43857bd85caada4b37a90489ca04b1a3d
mawia <92118320+39LPeter@users.noreply.github.com>
2024-08-16 15:23:49 +0300

    Update main.ts

--- 0x04-TypeScript/task_3/main.ts
+++ 0x04-TypeScript/task_3/main.ts
@@ -1,67 +1,12 @@
-interface Student {
-  firstName: string,
-  lastName: string,
-  age: number,
-  location: string
-}
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-const student1: Student = {
-  firstName: 'Paul',
-  lastName: 'Jerry',
-  age: 27,
-  location: 'Nigeria'
-}
-
-const student2: Student = {
-  firstName: 'Jerry',
-  lastName: 'Parkerson',
-  age: 28,
-  location: 'Edo state'
-}
-
-const studentList: Array<Student> = [student1, student2];
-
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-const table: HTMLTableElement = document.createElement('table');
-const thead: HTMLTableSectionElement = document.createElement('thead');
-const th1: HTMLTableCellElement = document.createElement('th');
-const th2: HTMLTableCellElement = document.createElement('th');
-
-th1.innerText = 'First Name';
-th2.innerText = 'Location';
-th1.style.border = '1px solid gray';
-th2.style.border = '1px solid gray';
-th1.style.padding = '.5rem';
-th2.style.padding = '.5rem';
-table.style.border = '1px solid gray';
-table.style.borderCollapse = 'collapse';
-
-
-
-thead.append(th1);
-thead.append(th2);
-
-table.append(thead);
-
-
-studentList.forEach((student) => {
-  const row: HTMLTableRowElement = document.createElement('tr');
-
-  const column1: HTMLTableCellElement = document.createElement('td');
-  const column2: HTMLTableCellElement = document.createElement('td');
-
-  column1.innerText = student.firstName;
-  column2.innerText = student.lastName;
-
-  column1.style.border = '1px solid gray';
-  column2.style.border = '1px solid gray';
-  column1.style.padding = '.5rem';
-  column2.style.padding = '.5rem';
-
-  row.append(column1);
-  row.append(column2)
-
-  table.append(row);
-});
-
-body.append(table)
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {...row, age: 23};
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
